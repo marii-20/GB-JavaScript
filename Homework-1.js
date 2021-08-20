57afdee5dbfb5d820049b8753a2eb17917325559
Marii-20 <gmaria92@hotmail.com>
2021-08-20 13:42:56 +0300

    master

---
+++ Homework-1.js
@@ -0,0 +1,25 @@
+
+
+// 1.
+let Celsius = 15;
+let Farenheit = (9 / 5) * Celsius + 32;
+
+alert(Farenheit);
+
+
+// 2.
+let name = 'Vasily';
+let admin = 'admin';
+
+admin = name;
+
+alert(admin);
+
+
+
+// 3. 
+let Sum = 1000 + "108";
+
+alert(Sum);
+
+// Результат: 1000108 
